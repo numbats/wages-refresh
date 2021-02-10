b5737b1d5f4f84f1ba2d4e315c90a389b54030e9
Dewi-Amaliah <dama0007@student.monash.edu>
2021-02-10 17:31:52 +1100

    Change the treatment of extreme values

--- code_di_weighted.R
+++ code_di_weighted.R
@@ -1,5 +1,6 @@
 library(tidyverse)
 library(zoo)
+library(rstatix)
 
 
 # open the tidy raw data set
@@ -23,13 +24,35 @@ wages_demog_hs <- wages_demog  %>% filter(grepl("GRADE", hgc))
 ggplot(wages_demog_hs, aes(x = mean_hourly_wage)) +
   geom_boxplot()
 
-# replace the obs with mean_hourly_wage > 100 to be NA
+# replace the obs with mean_hourly_wage is.extreme == TRUE to be NA
 # since we will impute the missing values with the last observation carried forward (locf) for each id
 # we give the flag for that locf observation
 
+wages_demog_ext <- wages_demog_hs %>%
+  group_by(id) %>%
+  identify_outliers("mean_hourly_wage") %>%
+  filter(is.extreme == TRUE) %>%
+  select(year, id, is.extreme)
+
+# ggplot(wages_demog_ext) +
+#   geom_histogram(aes(x = mean_hourly_wage))
+
+#summary(wages_demog_ext$mean_hourly_wage)
+
+
+# filter(wages_demog_ext, id == 3)
+# filter(wages_demog_hs, id == 6885)
+
+wages_demog_hs <- left_join(wages_demog_hs, wages_demog_ext, by = c("id", "year"))
+
 wages_demog_hs <- wages_demog_hs %>%
-  mutate(mean_hourly_wage = ifelse(mean_hourly_wage > 100, NA, mean_hourly_wage),
+  mutate(is.extreme = ifelse(is.na(is.extreme), FALSE,
+                             is.extreme))
+
+wages_demog_hs <- wages_demog_hs %>%
+  mutate(mean_hourly_wage = ifelse(is.extreme == TRUE, NA, mean_hourly_wage),
          is_locf = ifelse(is.na(mean_hourly_wage), TRUE, FALSE))
+
 
 # doing locf
 
@@ -59,33 +82,38 @@ wages_demog_hs <- wages_demog_hs %>%
 
 # calculate the IQR to create the extreme value flag
 
-IQR_cut_off <- IQR(wages_demog_hs$mean_hourly_wage, na.rm = TRUE)*1.5
-q1 <- quantile(wages_demog_hs$mean_hourly_wage, 0.25, na.rm = TRUE)
-q3 <- quantile(wages_demog_hs$mean_hourly_wage, 0.75, na.rm = TRUE)
-
-lower_limit <- q1 - IQR_cut_off
-upper_limit <- q3 + IQR_cut_off
-
-wages_demog_hs <- wages_demog_hs %>%
-  mutate(is_extreme_val = ifelse(mean_hourly_wage < lower_limit |
-                          mean_hourly_wage > upper_limit, TRUE,
-                        FALSE)) %>%
-  mutate(flag = case_when(is_wm == TRUE & is_extreme_val == FALSE ~ 1,
-                          is_wm == TRUE & is_extreme_val == TRUE ~ 2,
-                          is_wm == FALSE & is_extreme_val == FALSE ~ 3,
-                          is_wm == FALSE & is_extreme_val == TRUE ~ 4))
-
-filtered <- filter(wages_demog_hs, is_extreme_val == FALSE)
-summary(filtered$mean_hourly_wage)
+# IQR_cut_off <- IQR(wages_demog_hs$mean_hourly_wage, na.rm = TRUE)*1.5
+# q1 <- quantile(wages_demog_hs$mean_hourly_wage, 0.25, na.rm = TRUE)
+# q3 <- quantile(wages_demog_hs$mean_hourly_wage, 0.75, na.rm = TRUE)
+#
+# lower_limit <- q1 - IQR_cut_off
+# upper_limit <- q3 + IQR_cut_off
+#
+# wages_demog_hs <- wages_demog_hs %>%
+#   mutate(is_extreme_val = ifelse(mean_hourly_wage < lower_limit |
+#                           mean_hourly_wage > upper_limit, TRUE,
+#                         FALSE)) %>%
+#   mutate(flag = case_when(is_wm == TRUE & is_extreme_val == FALSE ~ 1,
+#                           is_wm == TRUE & is_extreme_val == TRUE ~ 2,
+#                           is_wm == FALSE & is_extreme_val == FALSE ~ 3,
+#                           is_wm == FALSE & is_extreme_val == TRUE ~ 4))
+#
+# filtered <- filter(wages_demog_hs, is_extreme_val == FALSE)
+# summary(filtered$mean_hourly_wage)
 
 
 
 # rename and select the wages in tidy
 wages_hs2020_weighted <- wages_demog_hs %>%
-  select(id, year, mean_hourly_wage, age_1979, gender, race, hgc, hgc_i, yr_hgc, number_of_jobs, total_hours, is_wm, is_extreme_val, is_locf, flag)
+  select(id, year, mean_hourly_wage, age_1979, gender, race, hgc, hgc_i, yr_hgc, number_of_jobs, total_hours, is_wm, is.extreme, is_locf)
 
-save(wages_hs2020_weighted, file="wages_hs2020_weighted.rda")
+#save(wages_hs2020_weighted, file="wages_hs2020_weighted.rda")
 
 summary(wages_hs2020_weighted$mean_hourly_wage)
+
+ggplot(wages_hs2020_weighted) +
+  geom_line(aes(x = year,
+                y = mean_hourly_wage,
+                group = id), alpha = 0.1)
 
 
